64313e3e1f399da33683d2f87119bc8eafdbb6f6
CarloMicieli <carlo.micieli@gmail.com>
2025-08-24 15:56:03 +0200

    feat(catalog): add the catalog library crate

--- crates/app/src/main.rs
+++ crates/app/src/main.rs
@@ -1,3 +1,4 @@
 fn main() {
-    println!("Hello, world!");
+    let str1 = catalog::catalog_function();
+    println!("{}", str1);
 }
---
+++ crates/libs/catalog/src/lib.rs
@@ -0,0 +1,3 @@
+pub fn catalog_function() -> &'static str {
+    "This is the catalog function"
+}
